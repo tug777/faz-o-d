0cfe782eb270e2c5b2845c57229fbe0edef2b6ca
luiz <141868804+tug777@users.noreply.github.com>
2025-06-02 14:23:14 -0300

    33343

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,9 +1,9 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Em sua visão, IA é uma boa invenção ?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "é assustadora!",
                 afirmacao: [
                     "No início ficou com medo do que essa tecnologia pode fazer.",
                     "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
@@ -11,7 +11,7 @@ export const perguntas = [
                 proxima: 1,
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "é maravilhosa!",
                 afirmacao: [
                     "Quis saber como usar IA no seu dia a dia.",
                     "Pensou que IA pode ajudar em tarefas da sua vida."
